8bcfea7005e2d1ff4dd9f0d6984ada840598ff59
haorang <haorang@berkeley.edu>
2018-03-10 02:21:28 -0800

    ok

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,20 @@
-function mml(){
-	
-}
+$(document).ready(function(){
+		
+   $('#tamcon').hover(function(){
+   		$(this).css("background-image", 'url("assets/img/coloredTAM.svg")');
+
+   },
+   function(){
+   		$(this).css("background-image", 'url("assets/img/fillTAM.svg")')
+   });
+
+   $('#den').hover(function(){
+   		$(this).css("background-image", 'url("assets/img/coloredden.svg")');
+
+   },
+   function(){
+   		$(this).css("background-image", 'url("assets/img/filledden.svg")')
+   });
+
+
+});
