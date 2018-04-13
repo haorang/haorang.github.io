056c882d8e0dcbe71fda860e6d0d33abe2c6caa6
haorang <haorang@berkeley.edu>
2018-04-12 20:23:56 -0700

    recolor den and tam

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 		
    $('#tam').hover(function(){
-   		$(this).css("background-image", 'url("assets/img/coloredTAM5.svg")');
+   		$(this).css("background-image", 'url("assets/img/coloredTAM7.svg")');
 
    },
    function(){
@@ -9,7 +9,7 @@ $(document).ready(function(){
    });
 
    $('#den').hover(function(){
-   		$(this).css("background-image", 'url("assets/img/coloredden2.svg")');
+   		$(this).css("background-image", 'url("assets/img/coloredden4.svg")');
 
    },
    function(){
@@ -17,7 +17,7 @@ $(document).ready(function(){
    });
 
    $('#sd').hover(function(){
-         $(this).css("background-image", 'url("assets/img/sd2.svg")');
+         $(this).css("background-image", 'url("assets/img/sd5.svg")');
 
    },
    function(){
